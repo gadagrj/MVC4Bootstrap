0a166101e5f56877622633e4f0f858aad0b54f16
Ravi Gadag <rjgadag@gmail.com>
2013-04-11 12:22:35 +0530

    Created Project
    Demos for Twitter bootstrap. How to use twitter bootstrap for MVC or
    HTML pages

---
+++ MVCBootstrap/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCBootstrap.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult LayoutDemos()
+        {
+            return View();
+        }
+
+        public ActionResult FormDemos()
+        {
+            return View();
+        }
+
+        public ActionResult JavascirptDemos()
+        {
+            return View();
+        }
+    }
+}
